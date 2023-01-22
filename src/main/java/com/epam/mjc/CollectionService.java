c94bf0e8b1e0fce349b4458e36440118d62dc65a
bakhridinova <bakhridinovaleyla@gmmail.com>
2023-01-22 18:28:43 +0500

    updated CollectionService.java, pom.xml

--- src/main/java/com/epam/mjc/CollectionService.java
+++ src/main/java/com/epam/mjc/CollectionService.java
@@ -8,11 +8,11 @@ import java.util.stream.Collectors;
 public class CollectionService {
 
     public List<Integer> filterEvenNumbers(List<Integer> list) {
-        return list.stream().filter(elem -> elem % 2 == 0).toList();
+        return list.stream().filter(elem -> elem % 2 == 0).collect(Collectors.toList());
     }
 
     public List<String> toUpperCaseCollection(List<String> list) {
-        return list.stream().map(String::toUpperCase).toList();
+        return list.stream().map(String::toUpperCase).collect(Collectors.toList());
     }
 
     public Optional<Integer> findMax(List<Integer> list) {
